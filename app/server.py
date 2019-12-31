df9d7599d39c8a3ee31278cd5d04bb05c9d7f1dc
daringtrifles <58384237+daringtrifles@users.noreply.github.com>
2020-01-01 00:25:19 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,18 +8,13 @@ from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
-
-export_file_url = 'https://www.dropbox.com/s/vznmhfiulf4z1ic/export.pkl?raw=0'
-export_file_name = 'export.pkl'
-
+export_file_url = 'https://www.dropbox.com/s/vznmhfiulf4z1ic/export.pkl?raw=1'
+export_file_name = 'export (1).pkl'
 classes = ['Pop Art','Renaissance Art','Chinese Art','Impressionism','Cubism']
 path = Path(__file__).parent
-
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
-
-
 async def download_file(url, dest):
     if dest.exists(): return
     async with aiohttp.ClientSession() as session:
@@ -27,8 +22,6 @@ async def download_file(url, dest):
             data = await response.read()
             with open(dest, 'wb') as f:
                 f.write(data)
-
-
 async def setup_learner():
     await download_file(export_file_url, path / export_file_name)
     try:
@@ -41,20 +34,14 @@ async def setup_learner():
             raise RuntimeError(message)
         else:
             raise
-
-
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
-
-
 @app.route('/')
 async def homepage(request):
     html_file = path / 'view' / 'index.html'
     return HTMLResponse(html_file.open().read())
-
-
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
     img_data = await request.form()
