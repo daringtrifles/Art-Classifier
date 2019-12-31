4bd0b68f3fe37a9e0e3f3566c1a6ca7a933624ab
daringtrifles <58384237+daringtrifles@users.noreply.github.com>
2019-12-31 13:54:10 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,9 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Ak1Y3KMeeW6iJ50nJjGFAdJ8rd-mS7yJ'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
-classes = ['Pop Art','Renaissance Art','Chinese Art','Impressionism','Cubism']
+
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
