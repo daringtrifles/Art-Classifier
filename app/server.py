8ef04cdac7cc5e03aa0c57c0121b72392107c9d6
daringtrifles <58384237+daringtrifles@users.noreply.github.com>
2019-12-31 14:41:40 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,8 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
-
+return JSONResponse({‘result’: str(learn.predict(img)[0])})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
